3e9ad359079342452dc0d714c64a367968ab2da3
James Denholm <nod.helm@gmail.com>
2025-03-02 16:35:07 +1100

    Add more navigation links, show by user type

---
+++ src/F1Tipping/Pages/Tipping/FullScores.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace F1Tipping.Pages.Tipping
+{
+    public class FullScoresModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
