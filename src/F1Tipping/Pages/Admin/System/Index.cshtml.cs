3845c35ad7bb207ac61f20f78bf763213ab2e9e1
James Denholm <nod.helm@gmail.com>
2025-03-09 00:04:14 +1100

    Add rules page

---
+++ src/F1Tipping/Pages/Admin/System/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace F1Tipping.Pages.Admin.System
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ src/F1Tipping/Pages/Rules.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace F1Tipping.Pages
+{
+    public class RulesModel : PageModel
+    {
+        private readonly ILogger<RulesModel> _logger;
+
+        public RulesModel(ILogger<RulesModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+
+}
