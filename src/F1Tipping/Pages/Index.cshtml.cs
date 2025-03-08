a5eac28f1eacca5d9085c05192326c852bb9c620
James Denholm <nod.helm@gmail.com>
2025-03-08 20:12:02 +1100

    Set up app publishing and improve index page

--- src/F1Tipping/Pages/Index.cshtml.cs
+++ src/F1Tipping/Pages/Index.cshtml.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
 
 namespace F1Tipping.Pages
 {
@@ -19,6 +20,12 @@ namespace F1Tipping.Pages
         public Type? MainViewComponent { get; set; } = default;
         [BindProperty]
         public object? MainViewComponentParameters { get; set; } = default;
+        [BindProperty]
+        public string WelcomeTitle { get; set; } = default!;
+        [BindProperty]
+        public string WelcomeSubtitle { get; set; } = default!;
+        [BindProperty]
+        public DateTimeOffset NextEventDeadline { get; set; } = default!;
 
         public IndexModel(ILogger<IndexModel> logger, ModelDbContext modelDb, UserManager<IdentityUser<Guid>> userManager)
         {
@@ -27,11 +34,34 @@ namespace F1Tipping.Pages
             _userManager = userManager;
         }
 
-        public IActionResult OnGet()
+        public async Task<IActionResult> OnGet()
         {
-            if ((User?.Identity?.IsAuthenticated ?? false) && User.IsInRole("Player"))
+            //if ((User?.Identity?.IsAuthenticated ?? false) && User.IsInRole("Player"))
+            //{
+            //    return Redirect("Tipping");
+            //}
+
+            WelcomeTitle = "Welcome to the F1 Tipping Competition!";
+
+            var utcNow = DateTimeOffset.UtcNow;
+            NextEventDeadline = (await _modelDb.Events.ToArrayAsync())
+                .Where(e => e.TipsDeadline > utcNow)
+                .OrderBy(e => e.TipsDeadline)
+                .FirstOrDefault()?.TipsDeadline ?? DateTimeOffset.MaxValue;
+
+            IdentityUser<Guid>? dbUser = null;
+            if (User is not null)
             {
-                return Redirect("Tipping");
+                dbUser = await _userManager.GetUserAsync(User);
+            }
+
+            if (dbUser is not null)
+            {
+                WelcomeSubtitle = "Click \"Tipping\" above to tip some races!";
+            }
+            else
+            {
+                WelcomeSubtitle = "Make an account, log in, tip some races!";
             }
 
             return Page();
