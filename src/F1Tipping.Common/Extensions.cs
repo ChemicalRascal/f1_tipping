65fc90defceb10e66412c47181e54300d244ae78
James Denholm <nod.helm@gmail.com>
2025-03-29 23:39:51 +1100

    Add player name to account management page

---
+++ src/F1Tipping.Common/Extensions.cs
@@ -0,0 +1,10 @@
+﻿namespace F1Tipping.Common
+{
+    public static class Extensions
+    {
+        public static string? NullIfEmpty(this string value)
+        {
+            return string.IsNullOrEmpty(value) ? null : value;
+        }
+    }
+}
--- src/F1Tipping/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
+++ src/F1Tipping/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -1,14 +1,16 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
-#nullable disable
 
 using System;
 using System.ComponentModel.DataAnnotations;
 using System.Text.Encodings.Web;
 using System.Threading.Tasks;
+using F1Tipping.Common;
+using F1Tipping.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace F1Tipping.Areas.Identity.Pages.Account.Manage
 {
@@ -16,60 +18,56 @@ namespace F1Tipping.Areas.Identity.Pages.Account.Manage
     {
         private readonly UserManager<IdentityUser<Guid>> _userManager;
         private readonly SignInManager<IdentityUser<Guid>> _signInManager;
+        private readonly ModelDbContext _modelDb;
 
         public IndexModel(
             UserManager<IdentityUser<Guid>> userManager,
-            SignInManager<IdentityUser<Guid>> signInManager)
+            SignInManager<IdentityUser<Guid>> signInManager,
+            ModelDbContext modelDb)
         {
             _userManager = userManager;
             _signInManager = signInManager;
+            _modelDb = modelDb;
         }
 
-        /// <summary>
-        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
-        /// </summary>
-        public string Username { get; set; }
+        public string Username { get; set; } = string.Empty;
 
-        /// <summary>
-        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
-        /// </summary>
         [TempData]
-        public string StatusMessage { get; set; }
+        public string? StatusMessage { get; set; }
 
-        /// <summary>
-        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
-        /// </summary>
         [BindProperty]
-        public InputModel Input { get; set; }
+        public InputModel? Input { get; set; }
 
-        /// <summary>
-        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
-        /// </summary>
         public class InputModel
         {
-            /// <summary>
-            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-            ///     directly from your code. This API may change or be removed in future releases.
-            /// </summary>
             [Phone]
             [Display(Name = "Phone number")]
-            public string PhoneNumber { get; set; }
+            public string? PhoneNumber { get; set; }
+            [Display(Name = "First name")]
+            public required string FirstName { get; set; }
+            [Display(Name = "Last name")]
+            public string? LastName { get; set; }
+            [Display(Name = "Display name")]
+            public string? DisplayName { get; set; }
         }
 
         private async Task LoadAsync(IdentityUser<Guid> user)
         {
-            var userName = await _userManager.GetUserNameAsync(user);
-            var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
+            var userName = (await _userManager.GetUserNameAsync(user)) ?? string.Empty;
+            var phoneNumber = (await _userManager.GetPhoneNumberAsync(user));
+            var player = await _modelDb.Players.Where(p => p.AuthUserId == user.Id).FirstOrDefaultAsync();
+            if (player?.Details is null)
+            {
+                StatusMessage = "Unexpected error when trying to set player details.";
+            }
 
             Username = userName;
-
             Input = new InputModel
             {
-                PhoneNumber = phoneNumber
+                PhoneNumber = phoneNumber,
+                FirstName = player?.Details?.FirstName ?? string.Empty,
+                LastName = player?.Details?.LastName,
+                DisplayName = player?.Details?.DisplayName,
             };
         }
 
@@ -100,7 +98,7 @@ namespace F1Tipping.Areas.Identity.Pages.Account.Manage
             }
 
             var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
-            if (Input.PhoneNumber != phoneNumber)
+            if (Input!.PhoneNumber != phoneNumber)
             {
                 var setPhoneResult = await _userManager.SetPhoneNumberAsync(user, Input.PhoneNumber);
                 if (!setPhoneResult.Succeeded)
@@ -108,6 +106,20 @@ namespace F1Tipping.Areas.Identity.Pages.Account.Manage
                     StatusMessage = "Unexpected error when trying to set phone number.";
                     return RedirectToPage();
                 }
+            }
+
+            var player = await _modelDb.Players.Where(p => p.AuthUserId == user.Id).FirstOrDefaultAsync();
+            if (player?.Details is null)
+            {
+                StatusMessage = "Unexpected error when trying to set player details.";
+            }
+            else
+            {
+                player.Details.FirstName = Input.FirstName.Trim();
+                player.Details.LastName = Input.LastName?.Trim().NullIfEmpty();
+                player.Details.DisplayName = Input.DisplayName?.Trim().NullIfEmpty();
+                _modelDb.Update(player);
+                await _modelDb.SaveChangesAsync();
             }
 
             await _signInManager.RefreshSignInAsync(user);
