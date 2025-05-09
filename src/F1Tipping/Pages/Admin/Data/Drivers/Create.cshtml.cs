4691e4136388e83b4e52c5c56106cec4c95a73d4
James Denholm <nod.helm@gmail.com>
2025-05-10 07:51:01 +1000

    Add driver creation page

---
+++ src/F1Tipping/Pages/Admin/Data/Drivers/Create.cshtml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using F1Tipping.Data;
+using F1Tipping.Model;
+using F1Tipping.Pages.PageModels;
+using Microsoft.CodeAnalysis;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace F1Tipping.Pages.Admin.Data.Drivers
+{
+    public class CreateModel : AdminPageModel
+    {
+        private readonly ModelDbContext _context;
+
+        public CreateModel(ModelDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Driver Driver { get; set; } = default!;
+
+        public IActionResult OnGet()
+        {
+            Driver = new()
+            {
+                FirstName = string.Empty,
+                LastName = string.Empty,
+                Nationality = string.Empty,
+                Number = string.Empty,
+            };
+
+            return Page();
+        }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more information, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Driver).State = EntityState.Added;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!DriverExists(Driver.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool DriverExists(Guid id)
+        {
+            return _context.Drivers.Any(e => e.Id == id);
+        }
+    }
+}
