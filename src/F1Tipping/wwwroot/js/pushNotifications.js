b478f91034936d07dda27089290b769909ed5494
James Denholm <nod.helm@gmail.com>
2025-11-04 21:43:21 +1100

    Clean up push notification JS

--- src/F1Tipping/wwwroot/js/pushNotifications.js
+++ src/F1Tipping/wwwroot/js/pushNotifications.js
@@ -10,31 +10,36 @@ async function InitPushNotificationSub(permission) {
         permission = (await navigator.permissions.query({ name: "notifications" })).state;
     }
     if (permission === "denied") {
-        PushNotificationState = "denied";
+        PushNotificationState = "permission_denied";
         return;
     }
     if (permission !== "granted") {
+        PushNotificationState = "permission_pending";
         return;
     }
 
-    var sub = await getSubscription();
-    if (sub === null) {
-        sub = await makeSubscription();
-    }
+    var sub = await getSubscription() ?? await makeSubscription();
 
-    var aRes = $.ajax({
-        type: "POST",
-        url: pushApiPostUri,
-        data: JSON.stringify(
-        {
-            DeviceEndpoint: sub.endpoint,
-            PublicKey: bufferToString(sub.getKey("p256dh")),
-            AuthSecret: bufferToString(sub.getKey("auth")),
-        }),
-        contentType: "application/json; charset=utf-8",
-        dataType: "json",
-    });
-    console.log(await aRes);
+    try {
+        var result = await $.ajax({
+            type: "POST",
+            url: pushApiPostUri,
+            data: JSON.stringify(
+                {
+                    DeviceEndpoint: sub.endpoint,
+                    PublicKey: bufferToString(sub.getKey("p256dh")),
+                    AuthSecret: bufferToString(sub.getKey("auth")),
+                }),
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+        });
+        console.log("Push Notification subscription succeeded:", result.responseJSON.title, result.status);
+        PushNotificationState = "subscription_successful";
+    } catch (e) {
+        console.log("Push Notification subscription failed:", e.responseJSON.title, e.status);
+        PushNotificationState = "subscription_failed";
+        await removeSubscription();
+    }
 };
 
 async function RequestPushPermission() {
@@ -50,6 +55,10 @@ async function getSubscription() {
 async function makeSubscription() {
     const registration = await navigator.serviceWorker.getRegistration();
     return registration.pushManager.subscribe({ userVisibleOnly: true, applicationServerKey: vapidPublicKey });
+}
+
+async function removeSubscription() {
+    return await registration.pushManager.removeSubscription();
 }
 
 function bufferToString(buffer) {
