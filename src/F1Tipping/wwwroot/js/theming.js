4f63cfada730b3a7889e8423da25f53b0bcae711
James Denholm <nod.helm@gmail.com>
2026-02-08 23:04:09 +1100

    Add auto-detected dark/light mode

---
+++ src/F1Tipping/wwwroot/js/theming.js
@@ -0,0 +1,14 @@
+﻿if (window.matchMedia) {
+    const darkModeQuery = window.matchMedia('(prefers-color-scheme: dark)');
+    darkModeQuery.addEventListener("change", updateTheme);
+    updateTheme(darkModeQuery);
+}
+
+function updateTheme(darkModeQuery) {
+    var pageDataset = document.querySelector("html").dataset;
+    if (darkModeQuery && darkModeQuery.matches) {
+        pageDataset.bsTheme = "dark";
+    } else {
+        pageDataset.bsTheme = "light";
+    }
+}
